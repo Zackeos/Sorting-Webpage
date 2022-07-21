8749c698faaf9499395a96a24a6221488c5b1483
Zackeos <zackfsummer@gmail.com>
2022-07-21 15:31:38 +0100

    created insertion sort js file

--- script.js
+++ script.js
@@ -256,16 +256,14 @@ for (let x=1; x<heights.length; x++){
   currentpos = x
   //for each item, pull it back until it is either at the start or it is smaller than bar before it
   let n = 0;
-  while(currentpos>0 && heights[currentpos-1]<currentvalue && n < 100){
+  while(currentpos>0 && heights[currentpos-1]<currentvalue){
     //add swap to animations array
     insertionanimations.push([initial[heights[currentpos-1]],initial[heights[currentpos]]])
     heights[currentpos] = heights[currentpos-1]
     currentpos=currentpos-1
   heights[currentpos] = currentvalue
-  n++;
   }
 }
-alert(n);
 heights=temp
 
 
